ec9a035981b9d433af77e31fef92c3a315fefc26
kentin PERRAT <kentinperrat@gmail.com>
2026-09-08 10:08:42 +0200

    Ajout du portfolio

---
+++ animations.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card, index) => {
+        card.style.animationDelay = `${index * 0.3}s`;
+    });
+});
+
